6f14b555f9064452dbfaa0a0fa8b5963e1a5b615
Michael Fitzgerald <8347380+michael-fitzgerald@users.noreply.github.com>
2022-10-24 00:35:25 -0400

    show asset list diff between symphonies

--- src/app/components/symphony-view-node/symphony-view-node.component.ts
+++ src/app/components/symphony-view-node/symphony-view-node.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AssetFlyweight } from 'src/app/models/AssetFlyweight';
 import { SymphonyFlyweight } from 'src/app/models/SymphonyFlyweight';
 
 @Component({
@@ -13,9 +14,32 @@ export class SymphonyViewNodeComponent implements OnInit {
 
   constructor() { }
 
-  
-
   ngOnInit(): void {
+  }
+
+  isNewAsset (row : any, investable : boolean) : boolean {
+      if(!this.node.Parent?.Assets?.length) return false;
+      let asset = row as AssetFlyweight;
+      let prev = this.node.Parent.Assets.filter(x => x.Ticker == asset.Ticker && 
+        ((investable && x.IsInvest) || (!investable && x.IsCompare)))[0];
+      return !prev;
+  }
+
+  getDeletedAssets(investable : boolean) : AssetFlyweight[]{
+    let ret : AssetFlyweight[] = [];
+    if(!this.node.Parent?.Assets?.length) return ret; // there is no parent, return empty set
+    for(let pa of this.node.Parent?.Assets.filter(x =>  ((investable && x.IsInvest) || (!investable && x.IsCompare)))){
+      //foreach parent asset
+        let current = this.node.Assets.filter(x => x.Ticker == pa.Ticker &&
+          ((investable && x.IsInvest) || (!investable && x.IsCompare))
+          )[0];
+        if(!current){
+          //asset not found in the current node. 
+          ret.push(pa);
+        }
+        
+    }
+    return ret;
   }
 
 }
